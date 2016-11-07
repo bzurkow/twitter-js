df2d129363f927ef64782d229525186bb1e6cc28
Ben Zurkow <benjamin.zurkow@gmail.com>
2016-11-07 18:22:33 -0500

    up to but not including part 6

--- app.js
+++ app.js
@@ -2,17 +2,43 @@ const express = require('express')
 const app = express()
 const chalk = require('chalk')
 const volleyball = require('volleyball')
+const nunjucks = require('nunjucks')
 
-app.use(volleyball)
+
 
 // app.use(function (req, res, next){
 // 	console.log(req.method,req.path)
 // 	next()	
 // })
 
-app.use("/special/",function(req,res,next){
-	console.log(chalk.magenta("You have reached the special area"))
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+nunjucks.configure('views')
+app.set('view engine','html')
+app.engine('html', nunjucks.render)
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.use("/index",function(req,res,next){
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
+
+
+// app.use(volleyball)
+
+// app.use("/special/",function(req,res,next){
+// 	console.log(chalk.magenta("You have reached the special area"))
+
+// })
 
 
 // app.get(function(req,res,next){
